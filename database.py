b4e18b6826ac94cc8b1c026217a18d482b09f07e
ragnarok <ragnarok@ragnaroks-MacBook-Pro.local>
2022-06-16 18:21:23 -0500

    fix: delete unused code

--- database.py
+++ database.py
@@ -3,8 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-os.getenv('DB_URL')
-
 SQL_ALCHEMY_DATABASE = os.getenv('DB_URL')
 
 engine = create_engine(SQL_ALCHEMY_DATABASE, connect_args={
